a9a10eff926e6c4fa39dde9ee056b1739c16dd22
Scott M. Kroll <skroll@gmail.com>
2013-02-06 11:03:46 -0500

    Remove uesless files

--- bin/get_keychain_pass.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-import sys
-from skroll import security
-
-print security.get_keychain_pass( sys.argv[1], sys.argv[2] )
-
-
-
--- offlineimap.py
+++
@@ -1,4 +0,0 @@
-import keyring
-
-def get_password(account, service):
-    return keyring.get_password(service, account)
